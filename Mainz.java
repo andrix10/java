cd038e00f46e704acd49ff92302663df3ce95fc0
Aleksandr Vasilenko <vasilenko.alex93@gmail.com>
2016-09-16 08:27:33 -0700

    Better ways to print

--- Mainz.java
+++ Mainz.java
@@ -2,27 +2,26 @@ import java.util.Scanner;
 
 public class Mainz
 {
-    public static void main(String[] args) 
-    {
+	public static void main(String[] args) {
 		int k = 1,o = 0;
 		LinkedList<Integer> temp;
 		LinkedList<Integer> i =  new LinkedList<Integer>();
-        Scanner scan= new Scanner(System.in);
-        while ( k != 0 ) 
-        {
-            System.out.println( "Enter: \n" );
-            k = scan.nextInt();
-            i.Add( k );
-			//i.Print();
-//			System.out.println( "\n" + o + "\n" );
-        }
+		Scanner scan= new Scanner(System.in);
+		while ( k != 0 ) {
+			System.out.print( "Enter: " );
+			k = scan.nextInt();
+			i.Add( k );
+			// i.Print();
+			// System.out.println( "\n" + o + "\n" );
+        	}
+		
 		temp = i;
 		/*while ( temp.prev != null) 
 		{
 			temp = temp.prev;
 			System.out.println( "prev\n" );
 		}
-		//temp.Print();
+		// temp.Print();
 		while ( temp.next != null ) 
 		{
 			System.out.println( "next\n" );
@@ -30,7 +29,8 @@ public class Mainz
 			System.out.println( "\n" + o + "\n" );
 			temp = temp.next;
 		}*/
-		temp.Print();
-    }   
+		// temp.Print();
+		System.out.println( temp );
+	}
 }
 
